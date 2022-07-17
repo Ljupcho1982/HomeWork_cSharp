75db650113e6279cf00ca787bb181c53d95081be
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-07-17 17:55:04 +0200

    Quizz_ilegal_name_variable

---
+++ Quizz_ilegal_name_variable/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using static System.Console;
+
+
+var name_varibale = new List<string>()
+{
+"myvariableisgood","99flake", 
+"_floor",
+"time2getjiggywidit",
+"wrox.com"
+
+
+};
+
+Write("Which of these are ilegal names for variable:");
+
+foreach(var name in name_varibale)
+{
+    Write(name+ "\n\r");
+
+}
+
+Write("take a first guess: ");
+var a = ReadLine();
+
+Write("take a second guess:");
+
+
+
+var b = ReadLine();
+
+if (a == "99flake" && b== "wrox.com")
+{
+    Write("You choose correct");
+}
+else
+{
+    Write("You are wrong");
+    
+}
+
