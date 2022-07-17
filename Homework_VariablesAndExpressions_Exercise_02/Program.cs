6ce5cda1a306e2193aaa4aec05ccb7683a2aa97e
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-07-17 16:39:15 +0200

    Exercise 02

---
+++ Homework_VariablesAndExpressions_Exercise_02/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+//Exercise 02 is:
+
+// which of the following is not a legal variable name?
+// myvariableisgood
+// 99flake
+//_floor
+// time2getjiggywidit
+// wrox.com
+
+//Answer:
+
+Write("The name in the variable in C# can contain letters and underscore charcters \n\r  the first letter of the name must be character the underscore is not ilegal but it`s not recommended \n\r Based on the aforementione ilegal variables from the list are:'99flake',wrox.com \n\r beacuse the first begins number and the second one containes full stop ");
