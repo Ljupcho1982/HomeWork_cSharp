bd56e799362e12d79042045baaf0c89675a77307
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-07-17 15:53:49 +0200

    Exercise 1

---
+++ Homework_VariablesAndExpressions_Exercise_01/Homework_VariablesAndExpressions_Exercise_01/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+/* In the following code, how would you refer to the name great from code in the namespace
+fabulous? */
+
+//namespace fabulous
+//{
+//    // code in fabulous namspace
+//}
+//namespace super
+//{
+//    namespace smashing
+//    {
+//        // great name defined
+//    }
+//}
+//Answer:
+
+Write("The task was not clear to me in order to understand i search on goole \n\r i found the answer on:@https://quizlet.com/540658704/mis-midterm-review-flash-cards/, \n\r the answer is:super.smashing.great");
