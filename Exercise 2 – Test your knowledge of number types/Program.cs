ef4f2bf0d95d4833c370605a5c6d8d0223c651df
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-07-18 21:30:01 +0200

    Exercise 2

---
+++ Exercise 2 – Test your knowledge of number types/Program.cs
@@ -0,0 +1,14 @@
+﻿//What type would you choose for the following "numbers"?
+//1. A person's telephone number
+//2. A person's height
+//3. A person's age
+//4. A person's salary
+//5. A book's ISBN
+//6. A book's price
+//7. A book's shipping weight
+//8. A country's population
+//9. The number of stars in the universe
+//10. The number of employees in each of the small or medium businesses in the United
+//Kingdom (up to about 50,000 employees per business)
+
+Console.WriteLine($"height-decimal, salary-double, book's ISBN-int,book's price-double,book's \n\r shipping weight-decimal,country's population-unit, number of stars in the universe-nuint \n\r umber of employees in each of the small or medium businesses in the United //Kingdom (up to about 50,000 employees per business)- ushort");
